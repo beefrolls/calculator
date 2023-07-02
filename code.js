9d2649226d86d60dd68c8701dd37dc385e210aa3
SupraConstruct <83255856+beefrolls@users.noreply.github.com>
2023-07-02 14:30:57 +0800

    Updated code.js
    Tested computation code further to accept multiple & varied operations

--- code.js
+++ code.js
@@ -52,6 +52,16 @@ digits.forEach((digit) => {
     })
 })
 
+let opList = [];
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', function() {
+        opList.push(operator.textContent)
+        console.log(opList)
+    })
+})
+
+
 //Testing (***Currently working***)
 function compute(array) {
     let a = array[0];
@@ -71,6 +81,63 @@ function compute(array) {
     } else {
         console.log('You only inputted 2 digits')
         console.log(result)
+    }
+}
+
+//WORKING (SO FAR)
+let initResult;
+function computeInit(array) {
+    let a = array[0];
+    let b = array[1];
+    let bIndex;
+    if (opList[0] == '+') {
+        initResult = a + b;
+        console.log(initResult)
+    } else if (opList[0] == '-') {
+        initResult = a - b;
+        console.log(initResult)
+    } else if (opList[0] == 'x') {
+        initResult = a * b;
+        console.log(initResult)
+    } else if (opList[0] == '÷') {
+        initResult = a / b;
+        console.log(initResult)
+    }
+}
+
+//WORKING (SO FAR)
+function computeMain(array) {
+    if (array.length < 3) {
+        console.log(initResult)
+    } else if (array.length >= 3) {
+        let resultMain = initResult;
+        let bIndex = 1;
+        let opIndex = 0;
+        for (i = 0; i < array.length - 2; i++, bIndex++, opIndex++) {
+            a = resultMain;
+            let b = array[bIndex + 1];
+            if (opList[opIndex + 1] == '+') {
+                resultMain = a + b;
+                console.log(`This is a: ${a}`)
+                console.log(`This is b: ${b}`)
+                console.log(resultMain)
+            } else if (opList[opIndex + 1] == '-') {
+                resultMain = a - b;
+                console.log(`This is a: ${a}`)
+                console.log(`This is b: ${b}`)
+                console.log(resultMain)
+            } else if (opList[opIndex + 1] == 'x') {
+                resultMain = a * b;
+                console.log(`This is a: ${a}`)
+                console.log(`This is b: ${b}`)
+                console.log(resultMain)
+            } else if (opList[opIndex + 1] == '÷') {
+                resultMain = a / b;
+                console.log(`This is a: ${a}`)
+                console.log(`This is b: ${b}`)
+                console.log(resultMain)
+            }
+        }
     }
 }
 
